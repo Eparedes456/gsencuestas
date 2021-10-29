38292ed998ca846714399708724ae5e95aa6ba84
Eparedes456 <erickparedes456@gmail.com>
2021-10-29 17:00:28 -0500

    sadd

---
+++ android/app/src/main/kotlin/pe/gob/regionsanmartin/gsencuestas/MainActivity.kt
@@ -0,0 +1,6 @@
+package pe.gob.regionsanmartin.gsencuestas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import UIKit
 import Flutter
-import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,10 +8,6 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    GMSServices.provideAPIKey("AIzaSyB1VEU-5uWFBH1NkfbnoA3Ttq68JFl-HHI")
-    if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
-    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
